263b223bfbded01f455487cdc56efaa0058cfcb3
Yohance Williams-Rodriguez <balamyohance@gmail.com>
2024-02-04 23:08:31 -0600

    clock works. eliminated clock delays when switching windows/first time a clock is clicked. next up: add clock actions (delete, edit, stop, resume, etc)

---
+++ ClockInputView.swift
@@ -0,0 +1,8 @@
+//
+//  ClockInputView.swift
+//  countdown-timer
+//
+//  Created by Yohance Williams on 2/3/24.
+//
+
+import Foundation
---
+++ Stopwatch.swift
@@ -0,0 +1,8 @@
+//
+//  Stopwatch.swift
+//  countdown-timer
+//
+//  Created by Yohance Williams on 2/4/24.
+//
+
+import Foundation

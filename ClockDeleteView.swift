15fcf981b3e50c8a090084df2756657df94fad38
Yohance Williams-Rodriguez <balamyohance@gmail.com>
2024-02-14 09:07:28 -0600

    clickable surface of clocks buttons is now the entire area of the button + other changes... app fully functioal, styling is missing and configurations view

---
+++ ClockDeleteView.swift
@@ -0,0 +1,8 @@
+//
+//  ClockDeleteView.swift
+//  countdown-timer
+//
+//  Created by Yohance Williams on 2/6/24.
+//
+
+import Foundation
---
+++ ClockEditView.swift
@@ -0,0 +1,8 @@
+//
+//  ClockEditView.swift
+//  countdown-timer
+//
+//  Created by Yohance Williams on 2/8/24.
+//
+
+import Foundation

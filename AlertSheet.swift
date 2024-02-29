5108ecf5baca8176d1d552c6d3f7670ed7acc295
Yohance Williams-Rodriguez <balamyohance@gmail.com>
2024-02-29 01:08:01 -0600

    added sheets for errors, proper error handling, all sheets are now styled, other minor details were added too.

---
+++ AlertSheet.swift
@@ -0,0 +1,18 @@
+//
+//  AlertSheet.swift
+//  countdown-timer
+//
+//  Created by Yohance Williams on 2/28/24.
+//
+
+import SwiftUI
+
+struct AlertSheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AlertSheet()
+}
